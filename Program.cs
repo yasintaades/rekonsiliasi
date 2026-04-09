5cc1a29fcdbc68a886bd99b17a0ad17251a5b801
yasintaades <148522651+yasintaades@users.noreply.github.com>
2026-04-09 10:19:45 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,40 @@
+using Microsoft.AspNetCore.Http.Features;
 using Reconciliation.Api.Endpoints;
 
-var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
-// CORS
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy =>
+    // CORS
+    builder.Services.AddCors(options =>
     {
-        policy.AllowAnyOrigin()
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+        options.AddDefaultPolicy(policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
     });
-});
 
-var app = builder.Build();
+    builder.WebHost.ConfigureKestrel(options =>
+    {
+        options.Limits.MaxRequestBodySize = 104857600; // 100 MB
+    });
+        builder.Services.Configure<FormOptions>(options =>
+    {
+        options.MultipartBodyLengthLimit = 104857600;
+    });
 
-app.UseCors();
 
-// 🔥 panggil endpoint dari file lain
-app.MapReconciliationEndpoints();
+    var app = builder.Build();
 
-app.Run();
+    app.UseCors();
+
+    // 🔥 panggil endpoint dari file lain
+    app.MapReconB2BEndpoint();
+    app.MapGet("/", () => "API is running...");
+    app.MapGet("/reconciliations/upload/details", () => "Details endpoint is running...");
+    app.MapReconPOVEndpoints();
+
+// =====pemisah=====
+
+
+    app.Run();
